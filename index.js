715ee5649dd851d60e449310357bca275270abf0
Michael Williams <dinosaur@riseup.net>
2014-07-14 23:51:53 -0700

    move .toContext() to be a class method, fix property definitions with s/writeable/writable

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ var opendata = function opendata (schema) {
   // add schema to jjv
   jjv.addSchema(schema.id, schema);
 
+
+  // setup properties
+  var property = {
+    enumerable: true,
+    writable: true,
+  }
+
   //
   // define Data constructor
   //
@@ -20,29 +27,35 @@ var opendata = function opendata (schema) {
       throw new Error("invalid object passed to " + schema.id + " constructor.");
     }
 
-    var property = predefine(this, {
-      enumerable: true,
-      writeable: true,
-    });
+    //
+    // setup instance properties
+    //
+    var instanceProperty = predefine(this, property);
 
-    // set type property
-    property("type", schema.id);
+    instanceProperty("type", schema.id);
 
     // TODO handle nested schemas (by $ref)
 
     // define shallow properties
     _.forEach(obj, function (val, key) {
       // if not nested then
-      property(key, val);
+      instanceProperty(key, val);
     }.bind(this));
   };
+
+  //
+  // setup class properties
+  //
+  var classProperty = predefine(Data, property);
+  
+  classProperty("schema", schema);
 
   //
   // setup hidden properties
   //
   var hidden = {
     enumerable: false,
-    writeable: false
+    writable: false
   }
   // in class
   var classHidden = predefine(Data, hidden);
@@ -55,18 +68,50 @@ var opendata = function opendata (schema) {
   protoHidden("_schema", schema);
 
   //
-  // setup methods in prototype
+  // setup methods
   //
-  var method = predefine(Data.prototype, {
+  //
+  var classMethod = predefine(Data, {
     enumerable: false,
-    writeable: true,
+    writable: true,
+  });
+  
+  classMethod("toContext", function () {
+    var context = {
+      id: "@id",
+      type: "@type",
+    };
+    // get prefixes
+    _.forIn(this.schema.prefixes, function (val, key) {
+      if (_.isString(key) && _.isEmpty(key)) {
+        key = "@vocab";
+      }
+      context[key] = val;
+    });
+    // get top-level context
+    if (this.schema.context) {
+      context[this.schema.id] = this.schema.context;
+    }
+    // get property contexts
+    _.forIn(this.schema.properties, function (propSchema, propName) {
+      if (propSchema.context) {
+        context[propName] = propSchema.context;
+      }
+    });
+    // TODO merge context of nested objects
+    return context;
   });
 
-  method("validate", function (options) {
+  var protoMethod = predefine(Data.prototype, {
+    enumerable: false,
+    writable: true,
+  });
+
+  protoMethod("validate", function (options) {
     return jjv.validate(schema.id, this.toJSON())
   });
   
-  method("toJSON", function () {
+  protoMethod("toJSON", function () {
     // create object to return
     var json = {};
     // TODO set properties of nested objects
@@ -77,31 +122,11 @@ var opendata = function opendata (schema) {
     return json;
   });
   
-  method("toContext", function () {
-    var context = {
-      id: "@id",
-      type: "@type",
-    };
-    // get prefixes
-    _.forIn(this._schema.prefixes, function (val, key) {
-      if (_.isString(key) && _.isEmpty(key)) {
-        key = "@vocab";
-      }
-      context[key] = val;
-    });
-    // get top-level context
-    context[schema.id] = schema.context;
-    // get property contexts
-    _.forIn(this._schema.properties, function (propSchema, propName) {
-      if (propSchema.context) {
-        context[propName] = propSchema.context;
-      }
-    });
-    // TODO merge context of nested objects
-    return context;
+  protoMethod("toContext", function () {
+    return this.constructor.toContext()
   });
   
-  method("toJSONLD", function () {
+  protoMethod("toJSONLD", function () {
     return _.extend(this.toJSON(), {
       "@context": this.toContext(),
       type: schema.id,
